c30cf736ecedb4e6044afe6072cd23683a3a5398
Nick A <nakursk@gmail.com>
2024-09-06 15:14:12 +0300

    initial commit

---
+++ Classroom.cs
@@ -0,0 +1,8 @@
+﻿namespace SF.HW03.Task15._6._6
+{
+    public class Classroom
+    {
+        public List<string> Students = [];
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SF.HW03.Task15._6._6
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+                new Classroom { Students = { "Evgeniy", "Sergey", "Andrew" }, },
+                new Classroom { Students = { "Anna", "Viktor", "Vladimir" }, },
+                new Classroom { Students = { "Bulat", "Alex", "Galina" }, }
+            };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        /// <summary>
+        /// Собирает всех студентов из всех классов в один массив.
+        /// </summary>
+        /// <param name="classes">Массив классов, содержащих студентов.</param>
+        /// <returns>Массив имен всех студентов.</returns>
+        private static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes
+                .SelectMany(s => s.Students)
+                .ToArray();
+        }
+    }
+}
